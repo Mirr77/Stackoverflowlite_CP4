8a62f3f2acae6c1e599f05d6f766155d61ad25b3
Mirriam <mirrmaina@gmail.com>
2018-09-25 21:12:32 +0300

    [Chore ##160768132]: add styling

--- js/main.js
+++ js/main.js
@@ -7,8 +7,23 @@ $(window).on('scroll', function(){
     }
 });
 
+$(document).on('click', '.card.questions', function(){
+  $('.questions-container').animate({
+    width: '450px'
+  }, 500);
+  $('.my-questions').animate({
+    width: '380px'
+  }, 500)
+  $('.question-answers').fadeIn(600);
+  $(this).addClass('blue');
+  $('.card.questions').not(this).removeClass('blue');
+ 
+}
+);
+
 
  $(document).ready( function(){
+
     $('.popup').on('click', function(){
       if($(event.target).is("#close")){
         $(".cover").fadeOut('slow');
