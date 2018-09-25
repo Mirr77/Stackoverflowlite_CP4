860e403e1ab6b5345669b0ee71827fc124e7b4b3
Mirriam <mirrmaina@gmail.com>
2018-09-25 21:20:26 +0300

    [Feature #160091347]: Add code for get another users profile]

--- js/profile.js
+++ js/profile.js
@@ -1,22 +1,51 @@
 const profileUrl='http://localhost:5000/api/v1/auth/userdata';
 
-getdata =  (url) => {
+const profile = {
 
-            fetch(profileUrl, {
-              method: 'GET',
-              headers: {"Authorization":localStorage.getItem('token') }
-              })
-            .then(response => response.json())
-            .then(data => {
-              console.log('REs-data:', data);
-              mapProfile(data);
-              })
-            .catch(err => {
-                console.log(`Fetch Error: ${err}`);
-              });
-  };
+  getmydata: (url) => {
 
-mapProfile = (data) => {
+    let user_id = localStorage.getItem('user_id')
+    fetch(`${profileUrl}/${user_id}`, {
+      method: 'GET',
+      headers: {"Authorization":localStorage.getItem('token') }
+      })
+    .then(response => response.json())
+    .then(data => {
+      console.log('REs-data:', data);
+      mapmyProfile(data);
+      })
+    .catch(err => {
+        console.log(`Fetch Error: ${err}`);
+      });
+     },
+
+  getdata: $(document).on('click', '.posted', 
+            function getdata(){
+              let user_id = $(this).attr('user_id')
+              let stored_id = localStorage.getItem('user_id')
+              fetch(`${profileUrl}/${user_id}`, {
+                method: 'GET',
+                headers: {"Authorization":localStorage.getItem('token') }
+                })
+              .then(response => response.json())
+              .then(data => {
+                if(stored_id == user_id){
+                  window.location.href = "myaccount.html"
+                }
+                else{
+                  window.location.href = "profile.html"; 
+                  mapdata(data);
+                }
+                })
+              .catch(err => {
+                  console.log(`Fetch Error: ${err}`);
+                })
+               })
+
+};
+
+
+mapmyProfile = (data) => {
 
             const element = `<div class="card stats">
                                 <h1>${data.data.username}</h1>
@@ -30,3 +59,20 @@ mapProfile = (data) => {
             $('.profile-info').html(element);
 
    };
+
+
+mapdata = (data) => {
+
+  const element = `<div class="card stats">
+                      <h1>${data.data.username}</h1>
+                      <p>${data.data.email}</p>
+                      <h1>Statistics</h1>
+                      <p class="title">No of Questions asked:${data.data.questions.length} </p>
+                      <p class="title">No of Questions answered:${data.data.questions_answered}</p>
+                      <p class="title">No of answers given:${data.data.answers} </p>
+                  </div>`;
+  
+  $('.profile-info-other').html(element);
+
+};
+
